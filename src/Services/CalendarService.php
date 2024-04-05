5d24510565729c1a53dccbbc2dea51ab320e3f4f
George Muscat <georgemuscat@proton.me>
2024-04-06 02:47:09 +1100

    change timezone

--- src/Services/CalendarService.php
+++ src/Services/CalendarService.php
@@ -27,7 +27,7 @@ class CalendarService
      * Why Reykjavik?
      * It's a common trick to use it in cases like this as a substitute for UTC as it doesn't observe daylight savings.
      */
-    public const TIMEZONE = 'Atlantic/Reykjavik';
+    public const TIMEZONE = 'Australia/Sydney';
 
     /**
      * Calendar manifest store, by event type.
